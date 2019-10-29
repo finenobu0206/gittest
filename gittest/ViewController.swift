c12f9a0d9b7db01374789d8ccf44b0d719f785a7
finenobu0206 <finenobu0206@gmail.com>
2019-10-29 23:05:49 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by nobuharu-imaoka on 2019/10/29.
+//  Copyright © 2019 nobuharu-imaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
